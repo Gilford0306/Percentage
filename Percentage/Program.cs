74f55b51ed9684bea3313aba4f24fba446734dcd
Gilford0306 <Gilford0306@gmail.com>
2022-06-16 11:47:06 +0300

    Add project files.

---
+++ Percentage/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задание 2
+//Пользователь вводит с клавиатуры два числа. Первое 
+//число — это значение, второе число процент, который 
+//необходимо посчитать. Например, мы ввели с клавиатуры 
+//90 и 10. Требуется вывести на экран 10 процентов от 90. 
+//Результат: 9.
+
+int k;
+do
+{
+    Console.WriteLine("Input number");
+    double num = double.Parse(Console.ReadLine());
+    Console.WriteLine("Input percentage");
+    double per = double.Parse(Console.ReadLine());
+    Console.WriteLine("Result:");
+    Console.WriteLine((num/100)*per);
+    Console.WriteLine("Int key - 1 - to continue, 0 - to exit");
+    k = int.Parse(Console.ReadLine());
+} while (k == 1);
+return;
+
